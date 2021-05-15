9d7494f594f25777e2d104e0ef20aa4f99ad5e18
Muhibb Hussain <2535643h@student.gla.ac.uk>
2021-05-15 04:35:23 +0100

    Chapter 8

--- tango_with_django_project/tango_with_django_project/rango/forms.py
+++ tango_with_django_project/tango_with_django_project/rango/forms.py
@@ -3,7 +3,7 @@ from rango.models import Page, Category
 
 
 class CategoryForm(forms.ModelForm):
-    name = forms.CharField(max_length=Category.NAME_MAX_LENGTH,
+    name = forms.CharField(max_length=128,
                            help_text="Please enter the category name.")
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
     likes = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
@@ -15,11 +15,21 @@ class CategoryForm(forms.ModelForm):
 
 
 class PageForm(forms.ModelForm):
-    title = forms.CharField(max_length=Page.TITLE_MAX_LENGTH,
+    title = forms.CharField(max_length=128,
                             help_text="Please enter the title of the page.")
     url = forms.URLField(max_length=200,
                          help_text="Please enter the URL of the page.")
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        url = cleaned_data.get('url')
+
+        if url and not url.startswith('http://'):
+            url = 'http://' + url
+            cleaned_data['url'] = url
+
+            return cleaned_data
 
     class Meta:
         model = Page
